75f86c3364b2cdad8bd30bad97800179b272c80f
Arun0073 <114335635+Arun0073@users.noreply.github.com>
2024-07-11 11:47:42 +0530

    first commit

--- src/Components/Anubhav/Anubhav.js
+++ src/Components/Anubhav/Anubhav.js
@@ -1,30 +1,44 @@
-import React,{useContext} from 'react'
-import './Anubhav.css'
+import React, { useContext } from "react";
+import "./Anubhav.css";
 import { themeContext } from "../../Context";
 
 const Anubhav = () => {
   const theme = useContext(themeContext);
   const darkmode = theme.state.darkmode;
   return (
-    <div className='Anubhav' id='Anubhav'>
+    <div className="Anubhav" id="Anubhav">
       <div className="achievement">
-        <div className="circle" style={{color: darkmode?'var(--orange)':''}}>2+</div>
-        <span style={{color: darkmode?'white':''}}>Years</span>
+        <div
+          className="circle"
+          style={{ color: darkmode ? "var(--orange)" : "" }}
+        >
+          2+
+        </div>
+        <span style={{ color: darkmode ? "white" : "" }}>Years</span>
         <span>Experience</span>
       </div>
       <div className="achievement">
-      <div className="circle" style={{color: darkmode?'var(--orange)':''}}>15+</div>
-       <span style={{color: darkmode?'white':''}}>Completed </span>
+        <div
+          className="circle"
+          style={{ color: darkmode ? "var(--orange)" : "" }}
+        >
+          15+
+        </div>
+        <span style={{ color: darkmode ? "white" : "" }}>Completed </span>
         <span>Projects</span>
-        </div>
+      </div>
       <div className="achievement">
-      <div className="circle" style={{color: darkmode?'var(--orange)':''}}>2+</div>
-       <span style={{color: darkmode?'white':''}}>Companies</span>
-       <span>Work</span>
+        <div
+          className="circle"
+          style={{ color: darkmode ? "var(--orange)" : "" }}
+        >
+          2+
         </div>
-      
+        <span style={{ color: darkmode ? "white" : "" }}>Companies</span>
+        <span>Work</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Anubhav
+export default Anubhav;
